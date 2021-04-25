9846c16f2024eee8d5b6a098ad458c9910b4f447
himynameisoleg <perchykoleg@gmail.com>
2021-04-25 12:27:53 -0400

    Task 1 passing.

--- innerClassesAndLambdas/task1/src/Person.java
+++ innerClassesAndLambdas/task1/src/Person.java
@@ -10,81 +10,38 @@ public class Person {
         protected String name;
         protected double salary;
 
+        private Position(String name, double salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
         @Override
         public String toString() {
-            return "";
+            return name + ": " + salary;
         }
     }
-/*
-    Create a
-    member inner
+    private Position position;
 
-    class called Position with two
-            member variables:
-    a name
-    and a
-    salary.Override the
+    public Person(String name, String birthday) {
+        this.name = name;
+        this.birthDate = LocalDate.parse(birthday);
+    }
+    public int getAge() {
+        Period age = Period.between(LocalDate.now(), this.birthDate);
+        return Math.abs(age.getYears());
+    }
+    public void setPosition(String title, double salary) {
+        position =  new Position(title, salary);
+    }
 
-    toString()
+    public Position getPosition() {
+        return position;
+    }
 
-    method as
-    specified in
-    the instructions
-
-     TODO
-    Create a
-
-    member variable(of Person) of type Position.
-
-
-     TODO
-    Create a
-    constructor for
-    Person that
-    takes in
-    two Strings.
-    A name
-    and a
-    birthdate in
-
-    ISO format(yyyy-mm-dd) and sets the corresponding memeber variables correctly
-
-     TODO
-    Create a
-    getAge method
-    that returns
-    the age
-    in years
-    of this
-    person.You need
-    to use
-    the Period
-
-    class here.
-     TODO
-    Code the
-    setPosition method.
-    This should
-    take one
-    String and
-    one double for
-    the title
-    and the
-    salary respectively.
-
-    /* TODO
-    A getter for
-    position.
-
-            TODO
-            Override the
-
-    toString() method to display the person's name, age and position (with salary and title).
-     TODO
-    Finish closing
-    curly braces
-    and the
-    class.
-    */
-
+    @Override
+    public String toString() {
+        return  this.name + " " +
+                this.getAge() + " " +
+                getPosition().toString();
+    }
 }
