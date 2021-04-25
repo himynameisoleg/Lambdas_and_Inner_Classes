32680a17f1a6d4c4208851927050c1809151ac78
himynameisoleg <perchykoleg@gmail.com>
2021-04-25 15:20:44 -0400

    Task 3 passing.

--- innerClassesAndLambdas/task3/src/Person.java
+++ innerClassesAndLambdas/task3/src/Person.java
@@ -1,2 +1,44 @@
-/* TODO */
-Copy the Person class from Task 2 here. Copy->Paste
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+
+    private String name ;
+    private LocalDate birthDate;
+    private class Position {
+        protected String name;
+        protected double salary;
+
+        private Position(String name, double salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        @Override
+        public String toString() {
+            return name + ": " + salary;
+        }
+    }
+    private Position position;
+
+    public Person(String name, String birthday) {
+        this.name = name;
+        this.birthDate = LocalDate.parse(birthday);
+    }
+    public int getAge() {
+        Period age = Period.between(LocalDate.now(), this.birthDate);
+        return Math.abs(age.getYears());
+    }
+    public void setPosition(String title, double salary) {
+        position =  new Position(title, salary);
+    }
+    public Position getPosition() {
+        return position;
+    }
+    @Override
+    public String toString() {
+        return  this.name + " " +
+                this.getAge() + " " +
+                getPosition().toString();
+    }
+}
--- innerClassesAndLambdas/task3/src/Task.java
+++ innerClassesAndLambdas/task3/src/Task.java
@@ -1,6 +1,17 @@
 public class Task {
     public static void main(String[] args){
-/* TODO */
-Copy the Task from task 2 here and add experiences where the end years are not in order. Watch it print the resume with the dates in order from most recent to least.
+        Person p = new Person("Kim Possible","2000-01-01");
+        p.setPosition("Associate", 34000);
+        Resume r = new Resume(p);
+
+        Resume.Education education = r.new Education("NEIU", "Computer Science");
+        r.addEducation(education);
+        Resume.Experience experience1 = new Resume.Experience("Engineer", 2017, 2021);
+        r.addExperience(experience1);
+        r.addExperience(new Resume.Experience("Milkman", 1952, 1960));
+        r.addExperience(new Resume.Experience("Bartender", 1992, 1999));
+        r.addExperience(new Resume.Experience("Pilot", 2011, 2020));
+
+        System.out.println(r);
     }
 }
